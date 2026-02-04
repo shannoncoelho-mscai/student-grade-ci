dbdb91b1261fe2246043c9b00ac9456c6e06a5ee
shannon coelho <mscai2025.shannon@unigoa.ac.in>
2026-02-04 16:47:38 +0530

    optimized

--- test/test_grade_calculator.py
+++ test/test_grade_calculator.py
@@ -1,5 +1,5 @@
 import pytest
-from src.grade_calculator import calculate_grade
+from grade_calculator import calculate_grade
 
 def test_A_plus():
     grade = calculate_grade([95, 90, 92, 94, 93])
