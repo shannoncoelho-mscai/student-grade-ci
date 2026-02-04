2a51ab866a64217ce4546078afabb398462ba6cc
shannon coelho <mscai2025.shannon@unigoa.ac.in>
2026-02-04 16:53:37 +0530

    optimized

--- test/test_grade_calculator.py
+++ test/test_grade_calculator.py
@@ -5,18 +5,3 @@ def test_A_plus():
     grade = calculate_grade([95, 90, 92, 94, 93])
     assert grade == "A+"
 
-def test_A():
-    grade = calculate_grade([75, 78, 80, 76, 77])
-    assert grade == "A"
-
-def test_B():
-    grade = calculate_grade([60, 62, 65, 63, 64])
-    assert grade == "B"
-
-def test_C():
-    grade = calculate_grade([50, 52, 55, 53, 54])
-    assert grade == "C"
-
-def test_fail():
-    grade = calculate_grade([40, 45, 42, 43, 44])
-    assert grade == "Fail"
